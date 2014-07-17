fdd15fe1b3ef08c84795e05433774817f11a6ec9
clicktx <clicktx@gmail.com>
2014-07-17 19:13:55 +0900

    Add LC_Page_ShippingAdmin

---
+++ plg_ShippingAdmin_LC_Page_ShippingAdmin.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * ShippingAdmin
+ *
+ * Copyright(c) 2014 clicktx. All Rights Reserved.
+ *
+ * http://perl.no-tubo.net/
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// {{{ requires
+require_once CLASS_EX_REALDIR . 'page_extends/LC_Page_Ex.php';
+
+/**
+ * ヘルプ のページクラス.
+ *
+ * @package Page
+ * @author clicktx
+ * @version $Id: LC_Page_ShippingAdmin 1 2014-07-17 00:00:00Z $
+ */
+class plg_ShippingAdmin_LC_Page_ShippingAdmin extends LC_Page_Ex {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * Page を初期化する.
+     *
+     * @return void
+     */
+    function init() {
+        parent::init();
+        $this->tpl_title = '配送情報登録';
+    }
+
+    /**
+     * Page のプロセス.
+     *
+     * @return void
+     */
+    function process() {
+        parent::process();
+        $this->action();
+        $this->sendResponse();
+    }
+
+    /**
+     * Page のアクション.
+     *
+     * @return void
+     */
+    function action() {
+    }
+
+    /**
+     * デストラクタ.
+     *
+     * @return void
+     */
+    function destroy() {
+        parent::destroy();
+    }
+}
+?>
