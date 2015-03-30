99a9d3e15bc31ad6286c000a00c736dd3ff2fcd8
clicktx <clicktx@gmail.com>
2015-03-30 20:24:36 +0900

    Extract class Admin_Order_Status, hook action_after

--- ShippingAdmin.php
+++ ShippingAdmin.php
@@ -23,6 +23,7 @@
 
 // 管理
 require_once PLUGIN_UPLOAD_REALDIR . 'ShippingAdmin/class/admin/order/plg_ShippingAdmin_LC_Page_Admin_Order.php';
+require_once PLUGIN_UPLOAD_REALDIR . 'ShippingAdmin/class/admin/order/plg_ShippingAdmin_LC_Page_Admin_Order_Status.php';
 
 
 /**
@@ -246,7 +247,7 @@ class ShippingAdmin extends SC_Plugin_Base {
         $objHelperPlugin->addAction("LC_Page_Admin_Order_Edit_action_before", array(&$this, "push_arrShippingKeys"), $this->arrSelfInfo['priority']);
         $objHelperPlugin->addAction("LC_Page_Admin_Order_Disp_action_before", array(&$this, "push_arrShippingKeys"), $this->arrSelfInfo['priority']);
         $objHelperPlugin->addAction("LC_Page_Admin_Order_action_before", array(new plg_ShippingAdmin_LC_Page_Admin_Order(), "action_before"), $this->arrSelfInfo['priority']);
-        $objHelperPlugin->addAction("LC_Page_Admin_Order_Status_action_after", array(&$this, "admin_order_status_after"), $this->arrSelfInfo['priority']);
+        $objHelperPlugin->addAction("LC_Page_Admin_Order_Status_action_after", array(new plg_ShippingAdmin_LC_Page_Admin_Order_Status(), "action_after"), $this->arrSelfInfo['priority']);
 
         $objHelperPlugin->addAction("prefilterTransform", array(&$this, "prefilterTransform"), $this->arrSelfInfo['priority']);
         $objHelperPlugin->addAction("SC_FormParam_construct", array(&$this, "addParam"), $this->arrSelfInfo['priority']);
@@ -285,98 +286,6 @@ class ShippingAdmin extends SC_Plugin_Base {
         }
 
         return $mode;
-    }
-
-    /**
-    * 管理機能 対応状況管理 のページクラス.
-    *
-    * @param LC_Page_Admin_Order_Status $objPage 管理対応状況管理 のページクラス.
-    * @return void
-    */
-    function admin_order_status_after($objPage) {
-        $objPurchase = new SC_Helper_Purchase_Ex();
-
-        // 配送業者一覧を取得
-        $objPage->arrDeliv = SC_Helper_Delivery_Ex::getIDValueList();
-
-        // memo: オリジナルのコードと２重で実行することになるが...
-        // パラメーター管理クラス
-        $objFormParam = new SC_FormParam_Ex();
-        // パラメーター情報の初期化
-        $objPage->lfInitParam($objFormParam);
-        $objFormParam->setParam($_POST);
-        // 入力値の変換
-        $objFormParam->convParam();
-
-        switch ($objPage->getMode()) {
-            case 'plg_shippingadmin_update':
-                $changeStatus = $objFormParam->getValue('change_status');
-                $arrMoveOderId = $objFormParam->getValue('move');
-
-                switch ($changeStatus) {
-                    // 削除
-                    case 'delete':
-                        // 削除確認がチェックされているか？
-                        $del_check = $objFormParam->getValue('del_check');
-                        if ($del_check){
-                            $objPage->lfDelete($arrMoveOderId);
-                        } else {
-                            $objPage->tpl_onload = "window.alert('削除時は削除確認にチェックを入れて下さい。');";
-                        }
-
-                        break;
-                    // 発送済み
-                    case '5':
-                        // 発送済みに変更する場合は荷物追跡番号が登録されているかチェック
-                        $checkFlag = 0;
-                        foreach ($arrMoveOderId as $index => $order_id) {
-                            $arrShippings = $objPurchase->getShippings($order_id, false);
-                            $checkFlag += $this->lfCheckTrackingNo($arrShippings);
-                        }
-                        if ($checkFlag){
-                            $objPage->tpl_onload = "window.alert('荷物追跡番号が登録されていないため、選択項目を" . $arrORDERSTATUS[$changeStatus] . "へ移動出来ませんでした');";
-                            break;
-                        }
-                        // 正常の場合はbreak; しない
-                    // 更新
-                    default:
-                        $objPage->lfStatusMove($changeStatus, $arrMoveOderId);
-                        break;
-                }
-                break;
-
-            case 'search':
-            default:
-                break;
-        }
-
-        // 対応状況
-        $status = $objFormParam->getValue('status');
-        if (strlen($status) === 0) {
-                //デフォルトで新規受付一覧表示
-                $status = ORDER_NEW;
-        }
-        $objPage->SelectedStatus = $status;
-        // 検索結果の表示
-        $objPage->lfStatusDisp($status, $objFormParam->getValue('search_pageno'));
-        // 配送情報を代入
-        foreach ($objPage->arrStatus as $key_index => $value) {
-            $order_id = $objPage->arrStatus[$key_index]['order_id'];
-            $arrShippings = $objPurchase->getShippings($order_id, false);
-            $objPage->arrStatus[$key_index]['shippings'] = $arrShippings;
-        }
-    }
-
-    /**
-    * 荷物追跡番号が登録されているかチェック
-    * @param
-    */
-    function lfCheckTrackingNo(&$arrShippings){
-        $checkFlag = 0;
-        foreach ($arrShippings as $index => $shippings) {
-            if (!$shippings['plg_shippingadmin_tracking_no']){ ++$checkFlag; }
-        }
-        return $checkFlag;
     }
 }
 ?>
